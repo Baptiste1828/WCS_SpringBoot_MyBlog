e2c2fa58be53a324c9174592cbeb5ca449060393
Baptiste Delefosse <baptiste.delefosse@hotmail.fr>
2025-01-14 19:57:47 +0100

    Spring05 : initial commit

---
+++ src/main/java/org/wildcodeschool/myblog/controller/CategoryController.java
@@ -0,0 +1,50 @@
+package org.wildcodeschool.myblog.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.wildcodeschool.myblog.model.Category;
+import org.wildcodeschool.myblog.repository.CategoryRepository;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+
+        Category savedCategory = categoryRepository.save(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category categoryDetails) {
+
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        category.setName(categoryDetails.getName());
+        Category updatedCategory = categoryRepository.save(category);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable Long id) {
+
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        categoryRepository.delete(category);
+        return ResponseEntity.noContent().build();
+    }
+}
